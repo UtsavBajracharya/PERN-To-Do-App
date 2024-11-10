a4456c6f59c28bc9067ebbd8a0a8214d55652164
Utsav <utsavbajra619@gmail.com>
2024-11-10 11:48:50 -0500

    database configuration

---
+++ server/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE perntodo;
+
+-- \c perntodo
+
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
